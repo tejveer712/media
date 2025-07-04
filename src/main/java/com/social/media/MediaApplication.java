bc583f5e0c0ade4bc8e82a6b17e0132db499cdbd
Tejveer <tejveer712@gmail.com>
2025-07-04 17:28:55 +0530

    Update MediaApplication.java

--- src/main/java/com/social/media/MediaApplication.java
+++ src/main/java/com/social/media/MediaApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MediaApplication {
-
+//This is the main application file
 	public static void main(String[] args) {
 		SpringApplication.run(MediaApplication.class, args);
 	}
