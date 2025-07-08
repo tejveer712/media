c647b0916b9af406fe46835cfaf26b41b2061dd6
Tejveer <tejveer712@gmail.com>
2025-07-08 08:48:38 +0530

    code cleanup

--- src/main/java/com/social/media/MediaApplication.java
+++ src/main/java/com/social/media/MediaApplication.java
@@ -1,10 +1,7 @@
 package com.social.media;
 
-import com.social.media.models.SocialUser;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MediaApplication {
